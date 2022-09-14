659521df046c8ca651473796179640bcb9482e4d
guilhemeicarofr <guilhemeicarofr@gmail.com>
2022-09-14 04:47:18 -0300

    heroku deploy

--- src/database/db.js
+++ src/database/db.js
@@ -2,6 +2,7 @@ import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
 const mongo = await new MongoClient(process.env.MONGO_URI).connect();
 
 const db = mongo.db('mywallet');
